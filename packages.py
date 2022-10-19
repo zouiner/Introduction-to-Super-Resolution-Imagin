4085e73b2349047bdb3926b44dba84b9a229f78a
Patipol Saengduean <patipolsaengduean@nas-10-240-132-76.york.ac.uk>
2022-10-20 00:52:57 +0100

    Add package

--- packages.py
+++ packages.py
@@ -4,4 +4,5 @@ import numpy as np
 from skimage import io
 from skimage.transform import rescale, resize_local_mean
 from skimage.filters import correlate_sparse
+from skimage.restoration import inpaint
 from matplotlib import pyplot as plt
